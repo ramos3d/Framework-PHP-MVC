b70eb32622bbee4775da9a7a06bb76c6096e9e13
Ramos3d <marceloxramos3d@gmail.com>
2018-10-28 16:27:15 -0300

    Framework MVC - Routing & Structure

---
+++ app/controllers/Controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  This is the main Controller, as a result, all new controllers must to extend
+ *  from this Controller
+ */
+class Controller extends AnotherClass
+{
+
+    function __construct(argument)
+    {
+        # code...
+    }
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,2 @@
+<h1>View de Home</h1>
+<?=$data['name']?>
